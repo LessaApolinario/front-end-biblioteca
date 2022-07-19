3d3640d4f429466cabbc2b8ca42d3915b7b59b9a
Lessa Apolinario <lessaapolinario@gmail.com>
2022-07-19 11:19:53 -0300

    initial setup

---
+++ library-ifal/src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.module.scss';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
---
+++ library-ifal/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './ui/styles/globals.scss';
+
+import App from './app/App';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+    <App />
+);
