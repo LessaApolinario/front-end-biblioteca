6d2a07e03c69862a6d86614bbd59fdcf62e0ef8b
Lessa Apolinario <lessaapolinario@gmail.com>
2022-07-19 17:09:03 -0300

    Add title prop for Button component

--- library-ifal/src/ui/components/Button.tsx
+++ library-ifal/src/ui/components/Button.tsx
@@ -4,13 +4,15 @@ import styles from '../styles/components/Button.module.scss'
 
 interface ButtonProps {
   children: ReactNode
+  title?: string
   type?: 'button' | 'submit' | 'reset'
   onClick?(): void 
 }
 
-function Button({ children, type, onClick }: ButtonProps) {
+function Button({ children, title, type, onClick }: ButtonProps) {
   return (
     <button
+      title={title}
       className={styles.container}
       type={type}
       onClick={onClick}
