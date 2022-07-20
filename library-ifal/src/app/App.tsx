ab42f9083d8479bbc0990fcd7010a08fcf560de8
Lessa Apolinario <lessaapolinario@gmail.com>
2022-07-19 23:52:09 -0300

    Testing initial interface components

--- library-ifal/src/app/App.tsx
+++ library-ifal/src/app/App.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
+import Book from '../ui/components/Book';
+import BookFinder from '../ui/components/BookFinder';
+import BookList from '../ui/components/BookList';
+
 import styles from './App.module.scss';
 
 function App() {
   return (
     <div className={styles.container}>
-
+      <h1 style={{textAlign: 'center', fontSize: 64}}>Livros</h1>
+      <BookFinder />
+      <BookList>
+        <Book
+          title='1984'
+          author='Orwell,
+          George'
+          year='2009'
+          localization='823
+          O79m
+          ex.1'
+        />
+        <Book
+          title='1984'
+          year='2009'
+          localization='823
+          O79m
+          ex.2'
+        />
+      </BookList>
     </div>
   );
 }
