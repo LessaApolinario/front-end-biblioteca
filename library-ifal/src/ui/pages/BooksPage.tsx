f085c096c20b5c018063212df761a3cb47068254
Lessa Apolinario <lessaapolinario@gmail.com>
2022-07-24 20:30:20 -0300

    Add search books by title

--- library-ifal/src/ui/pages/BooksPage.tsx
+++ library-ifal/src/ui/pages/BooksPage.tsx
@@ -2,7 +2,8 @@ import React, { useCallback, useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import BookDTO from "../../core/dto/BookDTO";
-import api from "../../services/api";
+
+import { useFetch } from "../../hooks/useFetch";
 
 import Book from "../components/Book";
 import Button from "../components/Button";
@@ -13,59 +14,50 @@ import styles from '../styles/pages/BooksPage.module.scss'
 function BooksPage() {
   const inputRef = useRef<HTMLInputElement>(null)
   const formRef = useRef<HTMLFormElement>(null)
-  const [books, setBooks] = useState<BookDTO[]>([])
   const navigate = useNavigate()
+  const { data: books } = useFetch<BookDTO[]>('/book')
+  const [foundedBooks, setFoundedBooks] = useState<BookDTO[]>([])
 
-  const getBooks = async () => {
-    try {
-      const response = await api.get<BookDTO[]>('/book')
-      console.log('Promise:', response)
-      const { data, status, statusText } = response
-      console.log('Dados:',data)
-
-      if (status === 200 && statusText === 'OK') {
-        return data
-      }
-    } catch (error) {
-      console.log(error)
-    }
+  // TODO:: see if this function is really necessary
+  const insertBooks = (foundedBooks?: BookDTO[]) => {
+    return foundedBooks?.map(book => {
+      const { titulo, autor, edicao, ano, localizacao } = book
+      return <Book
+        key={titulo}
+        title={titulo}
+        author={autor}
+        edition={edicao}
+        year={ano}
+        localization={localizacao}
+        />
+    })
   }
-
-  const filterBooks = useCallback((books: BookDTO[], bookTitle: string) => {
-    return books.filter(({ title }) => title.toLowerCase() === bookTitle.toLowerCase())
-  }, [])
   
-  const insertBooks = useCallback((book: BookDTO, key: number) => {
-    const { title, author, edition, year, localization } = book
-    return <Book
-      key={key}
-      title={title}
-      author={author}
-      edition={edition}
-      year={year}
-      localization={localization}
-      />
-  }, [])
+  const loadBooksOnSearch = useCallback((inputRef: React.RefObject<HTMLInputElement>) => {
+      const query = inputRef.current?.value
   
-  const listBooks = useCallback((myInputRef: React.RefObject<HTMLInputElement>, books: BookDTO[]) =>{
-    const input = myInputRef.current
-    const title = input?.value
-    return title ? filterBooks(books, title) : []
-  }, [filterBooks])
-
-  const searchBooks = useCallback(async () => {
-    const books = await getBooks()
-
-    if (typeof books !== 'undefined') {
-      // const resolveAllBookPromises = await Promise.all(books)
-      console.log(books)
-      setBooks(books)
-    }
-  }, [])
+      if (query) {
+        const booksByTitle = books
+          ?.filter(({ titulo }) => titulo?.toUpperCase() === query.toUpperCase())
+        // const booksByAuthor = books
+        //   ?.filter(({ author }) => author.toUpperCase() === query.toUpperCase())
+        // const booksByEdition = books
+        //   ?.filter(({ edition })=> edition.toUpperCase() === query.toUpperCase())
+        // const booksByYear = books
+        //   ?.filter(({ year }) => year.toLowerCase() === query.toUpperCase())
+        
+        // const foundedBooks = booksByTitle || booksByAuthor || booksByEdition || booksByYear
+        // return foundedBooks
+        if (booksByTitle) {
+          setFoundedBooks(booksByTitle)
+        }
+      }
+    
+  }, [books])
 
   useEffect(() => {
-    searchBooks()
-  }, [searchBooks])
+    loadBooksOnSearch(inputRef)
+  }, [loadBooksOnSearch])
 
   function handleSubmit(event: React.FormEvent) {
     event.preventDefault()
@@ -94,7 +86,7 @@ function BooksPage() {
           />
 
           <div className={styles.buttons}>
-            <Button type='submit' onClick={() => searchBooks()}>Pesquisar livros</Button>
+            <Button type='submit' onClick={() => loadBooksOnSearch(inputRef)}>Pesquisar livros</Button>
             <Button type='submit' onClick={() => console.log('listAll')}>Listar livros</Button>
           </div>
         </div>
@@ -111,7 +103,17 @@ function BooksPage() {
           </tr>
         </thead>
         <tbody>
-          {books?.map((book, index) => insertBooks(book, index))}
+          {/* {insertBooks(foundedBooks)} */}
+          {foundedBooks?.map(book => {
+            return <Book
+              key={Math.random() * 9}
+              title={book.titulo}
+              author={book.autor}
+              edition={book.edicao}
+              year={book.ano}
+              localization={book.localizacao}
+            />
+          })}
         </tbody>
       </table>
     </div>
