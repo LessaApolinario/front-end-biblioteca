84e484ded7b81e6263d684dd543d010e0cd967e4
Lessa Apolinario <lessaapolinario@gmail.com>
2022-07-25 11:50:51 -0300

    Add scroll event for images div

--- library-ifal/src/ui/pages/HomePage.tsx
+++ library-ifal/src/ui/pages/HomePage.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { useRef } from "react";
 
 import { GiTreeBranch } from 'react-icons/gi'
 
@@ -11,9 +12,28 @@ import harry from '../../assets/img/harry.jpg'
 
 function HomePage() {
   const navigate = useNavigate()
+  const itemsRef = useRef<HTMLDivElement>(null)
 
   function redirectToBooks() {
     navigate('/books')
+  }
+
+  const scroll = (event: React.WheelEvent<HTMLDivElement>) => {
+    const items = itemsRef.current
+    
+    if (items) {
+      const isDivOrImg = 
+        event.target instanceof HTMLDivElement || 
+        event.target instanceof HTMLImageElement
+      
+        if (isDivOrImg) {
+        if (event.deltaY > 0) {
+          items.scrollBy(300, 0)
+        } else if (event.deltaY < 0) {
+            items.scrollBy(-300, 0)
+        }
+      }
+    }
   }
 
   return (
@@ -32,18 +52,40 @@ function HomePage() {
         <h1>Bem-vindo</h1>
 
         <div className={styles.carrousel}>
-          <div className={styles.images}>
-            <img src={harry} alt="" />
-            <img src={harry} alt="" />
-            <img src={harry} alt="" />
-            <img src={harry} alt="" />
-            <img src={harry} alt="" />
-            <img src={harry} alt="" />
-            <img src={harry} alt="" />
-            <img src={harry} alt="" />
-            <img src={harry} alt="" />
-            <img src={harry} alt="" />
-            <img src={harry} alt="" />
+          <div className={styles.images} ref={itemsRef} onWheel={event => scroll(event)}>
+            <div className={styles.item}>
+              <img onClick={() => navigate('/books')} src={harry} alt="" />
+            </div>
+            <div className={styles.item}>
+              <img onClick={() => navigate('/books')} src={harry} alt="" />
+            </div>
+            <div className={styles.item}>
+              <img onClick={() => navigate('/books')} src={harry} alt="" />
+            </div>
+            <div className={styles.item}>
+              <img onClick={() => navigate('/books')} src={harry} alt="" />
+            </div>
+            <div className={styles.item}>
+              <img onClick={() => navigate('/books')} src={harry} alt="" />
+            </div>
+            <div className={styles.item}>
+              <img onClick={() => navigate('/books')} src={harry} alt="" />
+            </div>
+            <div className={styles.item}>
+              <img onClick={() => navigate('/books')} src={harry} alt="" />
+            </div>
+            <div className={styles.item}>
+              <img onClick={() => navigate('/books')} src={harry} alt="" />
+            </div>
+            <div className={styles.item}>
+              <img onClick={() => navigate('/books')} src={harry} alt="" />
+            </div>
+            <div className={styles.item}>
+              <img onClick={() => navigate('/books')} src={harry} alt="" />
+            </div>
+            <div className={styles.item}>
+              <img onClick={() => navigate('/books')} src={harry} alt="" />
+            </div>
           </div>
         </div>
       </section>
