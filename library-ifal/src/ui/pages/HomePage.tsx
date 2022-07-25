9539d42fe5b1b4607666086794fda6633b01468a
Lessa Apolinario <lessaapolinario@gmail.com>
2022-07-25 09:31:47 -0300

    Add navigation bar and testing carousel

--- library-ifal/src/ui/pages/HomePage.tsx
+++ library-ifal/src/ui/pages/HomePage.tsx
@@ -7,6 +7,8 @@ import Header from "../components/Header";
 
 import styles from '../styles/pages/HomePage.module.scss'
 
+import harry from '../../assets/img/harry.jpg'
+
 function HomePage() {
   const navigate = useNavigate()
 
@@ -18,11 +20,32 @@ function HomePage() {
     <main className={styles.container}>
       <Header>
         <GiTreeBranch />
+        <ul>
+          <li onClick={() => navigate('/')}>Home</li>
+          <li onClick={() => navigate('/hints')}>Dicas</li>
+          <li onClick={() => navigate('/contact')}>Contato</li>
+        </ul>
         <Button type='button' onClick={redirectToBooks}>Acessar livros</Button>
       </Header>
-      
+
       <section className={styles.content}>
         <h1>Bem-vindo</h1>
+
+        <div className={styles.carrousel}>
+          <div className={styles.images}>
+            <img src={harry} alt="" />
+            <img src={harry} alt="" />
+            <img src={harry} alt="" />
+            <img src={harry} alt="" />
+            <img src={harry} alt="" />
+            <img src={harry} alt="" />
+            <img src={harry} alt="" />
+            <img src={harry} alt="" />
+            <img src={harry} alt="" />
+            <img src={harry} alt="" />
+            <img src={harry} alt="" />
+          </div>
+        </div>
       </section>
     </main>
   )
