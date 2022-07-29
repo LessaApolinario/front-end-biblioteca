8d1975ca6ba90bde4755a87e09cf2dd8352409cd
LessaApolinario <lessaapolinario@gmail.com>
2022-07-29 18:27:51 -0300

    Add bookTitle and authorName props

--- library-ifal/src/ui/components/Review.tsx
+++ library-ifal/src/ui/components/Review.tsx
@@ -1,19 +1,16 @@
-import BookDTO from '../../core/dto/BookDTO'
-
 import styles from '../styles/components/Review.module.scss'
 
 interface ReviewProps {
   username: string
-  book: BookDTO
+  bookTitle: string
+  authorName: string
   review: string
 }
 
-function Review({ username, book, review }: ReviewProps) {
-  const { titulo, autor } = book
-
+function Review({ username, bookTitle, authorName, review }: ReviewProps) {
   return (
     <article className={styles.container}>
-      <h3>{username} sobre {titulo} de {autor}:</h3>
+      <h3>{username} sobre {bookTitle} de {authorName}:</h3>
 
       <p className={styles.review}>
         {review}
