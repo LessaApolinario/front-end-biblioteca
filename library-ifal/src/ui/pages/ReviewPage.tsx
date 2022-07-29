8430a1625c6e1edc274facabf13a0e7cc1abf28a
LessaApolinario <lessaapolinario@gmail.com>
2022-07-29 19:06:10 -0300

    Add logic for add new review onto the page

--- library-ifal/src/ui/pages/ReviewPage.tsx
+++ library-ifal/src/ui/pages/ReviewPage.tsx
@@ -1,23 +1,138 @@
-import BookDTO from '../../core/dto/BookDTO'
-import User from '../../core/models/User'
-import Button from '../components/Button'
+import { createRef, useRef, useState } from 'react'
 
+import ReviewDTO from '../../core/dto/ReviewDTO'
+
+import Button from '../components/Button'
 import Review from '../components/Review'
 
 import styles from '../styles/pages/ReviewPage.module.scss'
 
 function ReviewPage() {
-  const user: User = {
-    username: 'Lessa',
-    password: 'asfknjjkashfjasdf' 
+  const username = 'Lessa'
+  const bookTitle = 'Core Java'
+  const authorName = 'Algum autor'
+
+  const initialReviews: ReviewDTO[] = [
+    {
+      username,
+      bookTitle,
+      authorName,
+      review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, esse magnam earum maxime fugiat voluptates, perspiciatis cupiditate, quos dolores reiciendis ipsa facere nam aspernatur fuga omnis molestias neque debitis sint!'
+    },
+    {
+      username,
+      bookTitle,
+      authorName,
+      review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, esse magnam earum maxime fugiat voluptates, perspiciatis cupiditate, quos dolores reiciendis ipsa facere nam aspernatur fuga omnis molestias neque debitis sint!'
+    },
+    {
+      username,
+      bookTitle,
+      authorName,
+      review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, esse magnam earum maxime fugiat voluptates, perspiciatis cupiditate, quos dolores reiciendis ipsa facere nam aspernatur fuga omnis molestias neque debitis sint!'
+    },
+    {
+      username,
+      bookTitle,
+      authorName,
+      review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, esse magnam earum maxime fugiat voluptates, perspiciatis cupiditate, quos dolores reiciendis ipsa facere nam aspernatur fuga omnis molestias neque debitis sint!'
+    },
+    {
+      username,
+      bookTitle,
+      authorName,
+      review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, esse magnam earum maxime fugiat voluptates, perspiciatis cupiditate, quos dolores reiciendis ipsa facere nam aspernatur fuga omnis molestias neque debitis sint!'
+    },
+    {
+      username,
+      bookTitle,
+      authorName,
+      review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, esse magnam earum maxime fugiat voluptates, perspiciatis cupiditate, quos dolores reiciendis ipsa facere nam aspernatur fuga omnis molestias neque debitis sint!'
+    },
+    {
+      username,
+      bookTitle,
+      authorName,
+      review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, esse magnam earum maxime fugiat voluptates, perspiciatis cupiditate, quos dolores reiciendis ipsa facere nam aspernatur fuga omnis molestias neque debitis sint!'
+    },
+    {
+      username,
+      bookTitle,
+      authorName,
+      review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, esse magnam earum maxime fugiat voluptates, perspiciatis cupiditate, quos dolores reiciendis ipsa facere nam aspernatur fuga omnis molestias neque debitis sint!'
+    },
+    {
+      username,
+      bookTitle,
+      authorName,
+      review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, esse magnam earum maxime fugiat voluptates, perspiciatis cupiditate, quos dolores reiciendis ipsa facere nam aspernatur fuga omnis molestias neque debitis sint!'
+    },
+    {
+      username,
+      bookTitle,
+      authorName,
+      review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, esse magnam earum maxime fugiat voluptates, perspiciatis cupiditate, quos dolores reiciendis ipsa facere nam aspernatur fuga omnis molestias neque debitis sint!'
+    },
+    {
+      username,
+      bookTitle,
+      authorName,
+      review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, esse magnam earum maxime fugiat voluptates, perspiciatis cupiditate, quos dolores reiciendis ipsa facere nam aspernatur fuga omnis molestias neque debitis sint!'
+    },
+    {
+      username,
+      bookTitle,
+      authorName,
+      review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, esse magnam earum maxime fugiat voluptates, perspiciatis cupiditate, quos dolores reiciendis ipsa facere nam aspernatur fuga omnis molestias neque debitis sint!'
+    }
+  ]
+
+  const [reviews, setReviews] = useState<ReviewDTO[]>(initialReviews)
+  const buttonRef = createRef<HTMLButtonElement>()
+  const bookTitleRef = useRef<HTMLInputElement>(null)
+  const authorNameRef = useRef<HTMLInputElement>(null)
+  const reviewTextareaRef = useRef<HTMLTextAreaElement>(null)
+
+  const handleFields = () => {
+    const bookInput = bookTitleRef.current
+    const authorInput = authorNameRef.current
+    const reviewTextarea = reviewTextareaRef.current
+
+    if (bookInput && authorInput && reviewTextarea) {
+      const bookTitle = bookInput.value
+      const authorName = authorInput.value
+      const review = reviewTextarea.value
+
+      return {
+        bookTitle,
+        authorName,
+        review
+      }
+    }
   }
 
-  const book: BookDTO = {
-    ano: '2011',
-    autor: 'Algum autor',
-    edicao: 'dfsdfsdf',
-    localizacao: 'kdkdkdkdk',
-    titulo: 'Core Java'    
+  const handleAddReview = () => {
+    const button = buttonRef.current
+
+    if (button) {
+      const newReview = handleFields()
+
+      if (newReview) {
+        const { bookTitle, authorName, review } = newReview
+
+        if (bookTitle !== '' && authorName !== '' && review !== '') {
+          setReviews((previousReviews) => [
+            {
+              username,
+              bookTitle,
+              authorName,
+              review
+            },
+            ...previousReviews
+          ])
+        }
+      }
+    }
   }
 
   return (
@@ -29,105 +144,38 @@ function ReviewPage() {
 
         <div className={styles.book}>
           <label>Livro</label>
-          <input type="text" />
+          <input type="text" ref={bookTitleRef} />
         </div>
 
         <div className={styles.author}>
           <label>Autor</label>
-          <input type="text" />
+          <input type="text" ref={authorNameRef} />
         </div>
         
         <div className={styles.review}>
           <label>Resenha</label>
-          <textarea cols={30} rows={5}></textarea>
+          <textarea cols={30} rows={5} ref={reviewTextareaRef}></textarea>
         </div>
 
-        <Button type='button' btnType='secondary'>Escrever</Button>
+        <Button 
+          ref={buttonRef} 
+          type='button' 
+          btnType='secondary' 
+          onClick={handleAddReview}
+        >
+          Escrever
+        </Button>
       </form>
       <div className={styles.reviews}>
-        <Review
-          user={user}
-          key={12312312}
-          book={book}
-          review={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum laudantium provident harum porro quod modi nisi iste quia consequuntur recusandae adipisci temporibus, earum incidunt unde. Necessitatibus libero cum voluptatum dicta?'}
-        />
-
-        <Review
-          user={user}
-          key={12312312}
-          book={book}
-          review={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum laudantium provident harum porro quod modi nisi iste quia consequuntur recusandae adipisci temporibus, earum incidunt unde. Necessitatibus libero cum voluptatum dicta?'}
-        />
-
-        <Review
-          user={user}
-          key={12312312}
-          book={book}
-          review={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum laudantium provident harum porro quod modi nisi iste quia consequuntur recusandae adipisci temporibus, earum incidunt unde. Necessitatibus libero cum voluptatum dicta?'}
-        />
-
-        <Review
-          user={user}
-          key={12312312}
-          book={book}
-          review={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum laudantium provident harum porro quod modi nisi iste quia consequuntur recusandae adipisci temporibus, earum incidunt unde. Necessitatibus libero cum voluptatum dicta?'}
-        />
-
-        <Review
-          user={user}
-          key={12312312}
-          book={book}
-          review={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum laudantium provident harum porro quod modi nisi iste quia consequuntur recusandae adipisci temporibus, earum incidunt unde. Necessitatibus libero cum voluptatum dicta?'}
-        />
-
-        <Review
-          user={user}
-          key={12312312}
-          book={book}
-          review={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum laudantium provident harum porro quod modi nisi iste quia consequuntur recusandae adipisci temporibus, earum incidunt unde. Necessitatibus libero cum voluptatum dicta?'}
-        />
-
-        <Review
-          user={user}
-          key={12312312}
-          book={book}
-          review={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum laudantium provident harum porro quod modi nisi iste quia consequuntur recusandae adipisci temporibus, earum incidunt unde. Necessitatibus libero cum voluptatum dicta?'}
-        />
-
-        <Review
-          user={user}
-          key={12312312}
-          book={book}
-          review={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum laudantium provident harum porro quod modi nisi iste quia consequuntur recusandae adipisci temporibus, earum incidunt unde. Necessitatibus libero cum voluptatum dicta?'}
-        />
-
-        <Review
-          user={user}
-          key={12312312}
-          book={book}
-          review={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum laudantium provident harum porro quod modi nisi iste quia consequuntur recusandae adipisci temporibus, earum incidunt unde. Necessitatibus libero cum voluptatum dicta?'}
-        />
-
-        <Review
-          user={user}
-          key={12312312}
-          book={book}
-          review={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum laudantium provident harum porro quod modi nisi iste quia consequuntur recusandae adipisci temporibus, earum incidunt unde. Necessitatibus libero cum voluptatum dicta?'}
-        />
-
-        <Review
-          user={user}
-          key={12312312}
-          book={book}
-          review={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum laudantium provident harum porro quod modi nisi iste quia consequuntur recusandae adipisci temporibus, earum incidunt unde. Necessitatibus libero cum voluptatum dicta?'}
-        />
-
-        <Review
-          user={user}
-          key={12312312}
-          book={book}
-          review={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum laudantium provident harum porro quod modi nisi iste quia consequuntur recusandae adipisci temporibus, earum incidunt unde. Necessitatibus libero cum voluptatum dicta?'}
-        />
+        {reviews?.map(({ username, bookTitle, authorName, review }) => (
+          <Review
+            key={Math.random() * 6}
+            username={username}
+            bookTitle={bookTitle}
+            authorName={authorName}
+            review={review}
+          />
+        ))}
       </div>
     </div>
   )
