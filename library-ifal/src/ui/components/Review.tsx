291565b19bd6a5fa912d912f1b391d020b8ad725
LessaApolinario <lessaapolinario@gmail.com>
2022-07-29 21:25:54 -0300

    Add logic to add new className into paragraph

--- library-ifal/src/ui/components/Review.tsx
+++ library-ifal/src/ui/components/Review.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from 'react'
+
 import styles from '../styles/components/Review.module.scss'
 
 interface ReviewProps {
@@ -8,11 +10,26 @@ interface ReviewProps {
 }
 
 function Review({ username, bookTitle, authorName, review }: ReviewProps) {
+  const paragraphRef = useRef<HTMLParagraphElement>(null)
+ 
+  useEffect(() => {
+    const paragraph = paragraphRef.current
+
+    if (paragraph) {
+      if (
+        paragraph.textContent?.length && 
+        paragraph.textContent?.length >= 500
+      ) {
+        paragraph.classList.add(`${styles['hiddenText']}`)
+      }
+    }
+  }, [])
+
   return (
     <article className={styles.container}>
       <h3>{username} sobre {bookTitle} de {authorName}:</h3>
 
-      <p className={styles.review}>
+      <p className={styles.review} ref={paragraphRef}>
         {review}
       </p>
     </article>
