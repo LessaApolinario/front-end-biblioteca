730ac63dd6be397fd62888752c611763f4035a17
LessaApolinario <lessaapolinario@gmail.com>
2022-07-29 22:37:58 -0300

    Add navigate link for review route

--- library-ifal/src/ui/pages/ReviewPage.tsx
+++ library-ifal/src/ui/pages/ReviewPage.tsx
@@ -227,6 +227,7 @@ function ReviewPage() {
             bookTitle={bookTitle}
             authorName={authorName}
             review={review}
+            onClick={() => navigate(`/reviews/${username}`)}
           />
         ))}
       </div>
