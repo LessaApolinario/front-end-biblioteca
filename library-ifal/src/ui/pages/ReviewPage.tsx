f9b36249a7ca4701d824b061d0d694f7690baa50
LessaApolinario <lessaapolinario@gmail.com>
2022-07-29 21:49:44 -0300

    Add new header rules for show form

--- library-ifal/src/ui/pages/ReviewPage.tsx
+++ library-ifal/src/ui/pages/ReviewPage.tsx
@@ -1,9 +1,14 @@
-import { createRef, useRef, useState } from 'react'
+import { createRef, useEffect, useRef, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import ReviewDTO from '../../core/dto/ReviewDTO'
 
 import Button from '../components/Button'
 import Review from '../components/Review'
+import Header from '../components/Header'
+
+import { BsPlusCircleFill } from 'react-icons/bs'
+import { GiTreeBranch } from 'react-icons/gi'
 
 import styles from '../styles/pages/ReviewPage.module.scss'
 
@@ -13,6 +18,18 @@ function ReviewPage() {
   const authorName = 'Algum autor'
 
   const initialReviews: ReviewDTO[] = [
+    {
+      username,
+      bookTitle,
+      authorName,
+      review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus nihil maiores dolorum. Cupiditate, labore? Accusamus voluptatibus temporibus quam quae in illo praesentium mollitia, fuga pariatur ea nulla nihil tempore culpa. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab iusto repudiandae neque repellendus. Mollitia blanditiis quasi eaque vitae iure ab, autem commodi earum, dignissimos saepe quam incidunt repellat fuga sunt! Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis fugiat, laborum rerum qui exercitationem, molestias harum dolores culpa fuga, ex officia sapiente alias modi fugit voluptas quia voluptatem facilis doloribus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos iusto eius nostrum maiores molestias voluptas rem ducimus eos, hic nulla enim fuga magni alias ipsum in quia ratione delectus adipisci.'
+    },
+    {
+      username,
+      bookTitle,
+      authorName,
+      review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus nihil maiores dolorum. Cupiditate, labore? Accusamus voluptatibus temporibus quam quae in illo praesentium mollitia, fuga pariatur ea nulla nihil tempore culpa. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab iusto repudiandae neque repellendus. Mollitia blanditiis quasi eaque vitae iure ab, autem commodi earum, dignissimos saepe quam incidunt repellat fuga sunt! Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis fugiat, laborum rerum qui exercitationem, molestias harum dolores culpa fuga, ex officia sapiente alias modi fugit voluptas quia voluptatem facilis doloribus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos iusto eius nostrum maiores molestias voluptas rem ducimus eos, hic nulla enim fuga magni alias ipsum in quia ratione delectus adipisci.'
+    },
     {
       username,
       bookTitle,
@@ -88,6 +105,8 @@ function ReviewPage() {
   ]
 
   const [reviews, setReviews] = useState<ReviewDTO[]>(initialReviews)
+  const [isVisible, setIsVisible] = useState(false)
+  const navigate = useNavigate()
   const buttonRef = createRef<HTMLButtonElement>()
   const bookTitleRef = useRef<HTMLInputElement>(null)
   const authorNameRef = useRef<HTMLInputElement>(null)
@@ -130,6 +149,8 @@ function ReviewPage() {
             },
             ...previousReviews
           ])
+
+          setIsVisible(false)
         }
       }
     }
@@ -137,8 +158,38 @@ function ReviewPage() {
 
   return (
     <div className={styles.container}>
-      <h2>Resenhas</h2>
+      <Header>
+        <GiTreeBranch />
+        
+        <ul>
+          <li onClick={() => navigate('/')}>Home</li>
+          <li onClick={() => navigate('/genres')}>Gêneros</li>
+          <li onClick={() => navigate('/books')}>Livros</li>
+          <li onClick={() => navigate('/hints')}>Dicas</li>
+          <li onClick={() => navigate('/contact')}>Contato</li>
+        </ul>
 
+        <Button
+          type='button'
+          btnType='secondary'
+          onClick={() => navigate('/')}
+        >
+          Sair
+        </Button>
+      </Header>
+
+    <div className={styles.plusIcon}>
+      <BsPlusCircleFill
+        onClick={
+          () => setIsVisible(!isVisible)
+        }
+        title='Adicionar resenha'
+      />
+    </div>
+
+    <h2>Resenhas</h2>
+
+    {isVisible &&
       <form action="#" method='POST' className={styles.form}>
         <h3>Escreva sua resenha</h3>
 
@@ -166,6 +217,8 @@ function ReviewPage() {
           Escrever
         </Button>
       </form>
+    }
+
       <div className={styles.reviews}>
         {reviews?.map(({ username, bookTitle, authorName, review }) => (
           <Review
