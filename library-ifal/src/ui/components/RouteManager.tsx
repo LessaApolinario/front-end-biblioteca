d6e645212212b1afc7919933d7ec26ba9a81842e
LessaApolinario <lessaapolinario@gmail.com>
2022-07-31 22:19:22 -0300

    Add register route

--- library-ifal/src/ui/components/RouteManager.tsx
+++ library-ifal/src/ui/components/RouteManager.tsx
@@ -4,6 +4,7 @@ import BooksPage from "../pages/BooksPage"
 import Error404Page from "../pages/Error404Page"
 import HomePage from "../pages/HomePage"
 import LoginPage from "../pages/LoginPage"
+import RegisterPage from "../pages/RegisterPage"
 import ReviewDetailsPage from "../pages/ReviewDetailsPage"
 import ReviewPage from "../pages/ReviewPage"
 
@@ -14,6 +15,7 @@ function RouteManager() {
         <Route path="/home" element={<HomePage />} />
         <Route path="/books" element={<BooksPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/reviews" element={<ReviewPage />} />
         <Route path="/reviews/review/:username" element={<ReviewDetailsPage />} />
         <Route path="/" element={<Navigate replace to='/home' />} />
