eaf66d024d2a384e8efb22f9a6807c0d945872e3
LessaApolinario <lessaapolinario@gmail.com>
2022-07-31 21:41:50 -0300

    Add new keys for ReviewDTO interface

--- library-ifal/src/core/dto/ReviewDTO.ts
+++ library-ifal/src/core/dto/ReviewDTO.ts
@@ -1,8 +1,13 @@
-interface ReviewDTO  {
-  username: string
-  bookTitle: string
-  authorName: string
+interface ReviewDTO {
+  _id?: string
+  user_id?: string
+  name: string
+  title_book: string
+  writer: string
   review: string
+  available?: boolean
+  updated_at?: string
+  created_at?: string
 }
 
 export default ReviewDTO
