79849ee7bbec40ec2f8e5a326fbb31eafa3be28d
LessaApolinario <lessaapolinario@gmail.com>
2022-08-02 17:36:47 -0300

    Chnage route parameter

--- library-ifal/src/ui/components/RouteManager.tsx
+++ library-ifal/src/ui/components/RouteManager.tsx
@@ -19,7 +19,7 @@ function RouteManager() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/reviews" element={<ReviewPage />} />
-          <Route path="/reviews/review/:username" element={<ReviewDetailsPage />} />
+          <Route path="/reviews/review/:id" element={<ReviewDetailsPage />} />
           <Route path="/" element={<Navigate replace to='/home' />} />
           <Route path="/*" element={<Error404Page />} />
         </Routes>
