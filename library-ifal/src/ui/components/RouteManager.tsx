feaf131470e97a0f2f51c187b3f7f2ecc0199d74
LessaApolinario <lessaapolinario@gmail.com>
2022-08-02 18:02:58 -0300

    Add reviews context on routes

--- library-ifal/src/ui/components/RouteManager.tsx
+++ library-ifal/src/ui/components/RouteManager.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import AuthProvider from "../contexts/AuthCTX"
+import ReviewsProvider from "../contexts/ReviewsCTX"
 import BooksPage from "../pages/BooksPage"
 
 import Error404Page from "../pages/Error404Page"
@@ -13,16 +14,18 @@ function RouteManager() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/books" element={<BooksPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/reviews" element={<ReviewPage />} />
-          <Route path="/reviews/review/:id" element={<ReviewDetailsPage />} />
-          <Route path="/" element={<Navigate replace to='/home' />} />
-          <Route path="/*" element={<Error404Page />} />
-        </Routes>
+        <ReviewsProvider>
+          <Routes>
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/books" element={<BooksPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/reviews" element={<ReviewPage />} />
+            <Route path="/reviews/review/:id" element={<ReviewDetailsPage />} />
+            <Route path="/" element={<Navigate replace to='/home' />} />
+            <Route path="/*" element={<Error404Page />} />
+          </Routes>
+        </ReviewsProvider>
       </AuthProvider>
     </BrowserRouter>
   )
