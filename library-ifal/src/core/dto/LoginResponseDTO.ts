33f11e59f559e19e52496ce0461fd1afd1084964
Lessa Apolinario <lessaapolinario@gmail.com>
2022-08-04 18:02:14 -0300

    Add name key

--- library-ifal/src/core/dto/LoginResponseDTO.ts
+++ library-ifal/src/core/dto/LoginResponseDTO.ts
@@ -1,8 +1,9 @@
-interface LoginResponseDto {
+interface LoginResponseDTO {
   id: string
+  name: string
   access_token: string
   token_type: 'bearer'
   expires_in: number
 }
 
-export default LoginResponseDto
+export default LoginResponseDTO
