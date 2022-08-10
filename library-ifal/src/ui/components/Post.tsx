ce6f8b6cc813d4f37f011b7c64fb88a1d164c5ff
Lessa Apolinario <71906304+LessaApolinario@users.noreply.github.com>
2022-08-10 19:20:17 -0300

    Update Post.tsx

--- library-ifal/src/ui/components/Post.tsx
+++ library-ifal/src/ui/components/Post.tsx
@@ -10,9 +10,43 @@ interface PostProps {
 }
 
 function Post({ name, title, content, created_at }: PostProps) {
+  const abbreviateMonth = (month: string) => {
+    const abbreviatedMonths: Record<string, string> = {
+      '01': "Ja",
+      '02': "Fev",
+      '03': "Mar",
+      '04': "Abr",
+      '05': "Maio",
+      '06': "Jun",
+      '07': "Jul",
+      '08': "Ago",
+      '09': "Set",
+      '10': "Out",
+      '11': "Nov",
+      '12': "Dez"
+    }
+
+    return abbreviatedMonths[month] ?? 'Não existe um mês correspondente'
+  }
+
+  const formatDate = () => {
+    const date = created_at?.slice(0, 10)
+    const time = created_at?.slice(11, 16)
+
+    if (date && time) {
+      const [hours, minutes] = time?.split(':')
+      const [year, month, day] = date.split('-')
+
+      const abbreviatedMonth = abbreviateMonth(month)
+      return `${abbreviatedMonth} ${day} ${year} às ${hours}:${minutes}`
+    }
+
+    return ''
+  }
+  
   return (
     <article className={styles.container}>
-      <h3>{name} <span className={styles.date}>{created_at}</span></h3>
+      <h3>{name} <span className={styles.date}>{formatDate()}</span></h3>
       <h4 className={styles.postTitle}>{title}</h4>
       
       <p className={styles.content}>{content}</p>
