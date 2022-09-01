f382772f4cbc722d944bb85a32e2b884a331b184
Lessa Apolinario <lessaapolinario@gmail.com>
2022-09-01 16:08:35 -0300

    Add children prop

--- library-ifal/src/ui/components/Hint.tsx
+++ library-ifal/src/ui/components/Hint.tsx
@@ -1,15 +1,17 @@
+import { ReactNode } from 'react'
+
 import styles from '../styles/components/Hint.module.scss'
 
 interface HintProps {
   title: string
-  content: string
+  children: ReactNode
 }
 
-function Hint({ title, content }: HintProps) {
+function Hint({ title, children }: HintProps) {
   return (
     <section className={styles.container}>
       <h2>{title}</h2>
-      <p>{content}</p>
+      {children}
     </section>
   )
 }
