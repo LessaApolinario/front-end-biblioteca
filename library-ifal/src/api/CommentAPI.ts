e7357249d059f284c9ab3bf508e7fff1cc6a8e51
Lessa Apolinario <lessaapolnario@gmail.com>
2022-09-25 17:55:16 -0300

    Add heades for request

--- library-ifal/src/api/CommentAPI.ts
+++ library-ifal/src/api/CommentAPI.ts
@@ -2,7 +2,11 @@ import ICommentAPI from '../core/interfaces/api/ICommentAPI'
 
 class CommentAPI extends ICommentAPI {
   async create(name: string, email: string, comment: string): Promise<void> {
-    await this.client.post('/api/comments', JSON.stringify({name, email, comment }))
+    await this.client.post('/api/comments', JSON.stringify({ name, email, comment }), {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
   }
 }
 
