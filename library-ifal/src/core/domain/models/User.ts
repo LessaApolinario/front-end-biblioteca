e77b6c8a5d56375f09583a930492fe980278346d
Lessa Apolinario <lessaapolnario@gmail.com>
2022-09-25 16:08:24 -0300

    Add more User fields

--- library-ifal/src/core/domain/models/User.ts
+++ library-ifal/src/core/domain/models/User.ts
@@ -1,6 +1,9 @@
 class User {
   id?: string
   name?: string
+  username?: string
+  email?: string
+  password?: string
   access_token?: string
   token_type?: 'bearer'
   expires_in?: number
@@ -9,6 +12,9 @@ class User {
     const user = new User()
     user.id = String(json['id'])
     user.name = String(json['name'])
+    user.username = String(json['username'])
+    user.email = String(json['email'])
+    user.password = String(json['password'])
     user.access_token = String(json['access_token'])
     user.token_type = 'bearer'
     user.expires_in = Number(json['expires_in'])
@@ -19,6 +25,9 @@ class User {
     const json: Record<string, unknown> = {}
     json["id"] = this.id
     json["name"] = this.name
+    json["username"] = this.username
+    json["email"] = this.email
+    json["password"] = this.password
     json["access_token"] = this.access_token
     json["token_type"] = this.token_type
     json["expires_in"] = this.expires_in
