15419599787150d25ee85b884c13862fdcc17f0d
Lessa Apolinario <lessaapolnario@gmail.com>
2022-09-25 18:55:07 -0300

    Validating data for books from backend

--- library-ifal/src/api/BookAPI.ts
+++ library-ifal/src/api/BookAPI.ts
@@ -4,9 +4,30 @@ import IBookAPI from '../core/interfaces/api/IBookAPI'
 class BookAPI extends IBookAPI {
   async fetch(): Promise<Book[]> {
     const response = await this.client.get('/books')
-    const books: Book[] = response.data.map((item: Record<string, unknown>) =>
-      Book.fromJSON(item)
-    )
+    const books: Book[] = response.data.map((item: Record<string, unknown>) => {
+      if (!item["titulo"]) {
+        item["titulo"] = ''
+      }
+
+      if (!item["autor"]) {
+        item["autor"] = ''
+      }
+
+      if (!item["edicao"]) {
+        item["edicao"] = ''
+      }
+
+      if (!item["ano"]) {
+        item["ano"] = ''
+      }
+
+      if (!item["localizacao"]) {
+        item["localizacao"] = ''
+      }
+
+      return Book.fromJSON(item)
+    })
+    
 
     return books
   }
