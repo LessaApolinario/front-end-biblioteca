66c211d23f93b808bc83a13b84bc1f67e0f55fc3
Lessa Apolinario <lessaapolnario@gmail.com>
2022-09-24 23:32:29 -0300

    Add missing inheritance

--- library-ifal/src/core/interfaces/api/IPostAPI.ts
+++ library-ifal/src/core/interfaces/api/IPostAPI.ts
@@ -1,6 +1,7 @@
 import Post from '../../domain/models/Post'
+import IAPI from './IAPI'
 
-abstract class IPostAPI {
+abstract class IPostAPI extends IAPI {
   abstract fetch(): Promise<Post[]>
   abstract create(post: Post): Promise<void>
 }
