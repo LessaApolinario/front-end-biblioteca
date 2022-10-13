73157a6879a1ff1c3977cd7c5cff2469b62fc9ee
Lessa Apolinario <lessaapolnario@gmail.com>
2022-10-13 12:50:12 -0300

    reformat code

--- library-ifal/src/api/PostAPI.ts
+++ library-ifal/src/api/PostAPI.ts
@@ -5,25 +5,8 @@ class PostAPI extends IPostAPI {
   async fetch(): Promise<Post[]> {
     const response = await this.client.get('/api/posts')
     const posts: Post[] = response.data.map((item: Record<string, unknown>) => {
-      if (!item["user_name"]) {
-        item["user_name"] = ''
-      }
-
-      if (!item["title"]) {
-        item["title"] = ''
-      }
-
-      if (!item["content"]) {
-        item["content"] = ''
-      }
-
-      if (!item["created_at"]) {
-        item["created_at"] = ''
-      }
-      
       return Post.fromJSON(item)
-    }
-    )
+    })
 
     return posts
   }
