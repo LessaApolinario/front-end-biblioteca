6c181704858d3530f6274dd28e3f5470df4000b5
Lessa Apolinario <lessaapolnario@gmail.com>
2022-10-13 12:50:33 -0300

    remove fields checking

--- library-ifal/src/api/BookAPI.ts
+++ library-ifal/src/api/BookAPI.ts
@@ -5,29 +5,8 @@ class BookAPI extends IBookAPI {
   async fetch(): Promise<Book[]> {
     const response = await this.client.get('/books')
     const books: Book[] = response.data.map((item: Record<string, unknown>) => {
-      if (!item["titulo"]) {
-        item["titulo"] = ''
-      }
-
-      if (!item["autor"]) {
-        item["autor"] = ''
-      }
-
-      if (!item["edicao"]) {
-        item["edicao"] = ''
-      }
-
-      if (!item["ano"]) {
-        item["ano"] = ''
-      }
-
-      if (!item["localizacao"]) {
-        item["localizacao"] = ''
-      }
-
       return Book.fromJSON(item)
     })
-    
 
     return books
   }
