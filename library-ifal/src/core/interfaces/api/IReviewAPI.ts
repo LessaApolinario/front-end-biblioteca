e257446f1c86e65a9afdc539619263c2b98cbcf8
Lessa Apolinario <lessaapolinario@gmail.com>
2022-11-18 17:30:41 -0300

    Change the signature of the create method

--- library-ifal/src/core/interfaces/api/IReviewAPI.ts
+++ library-ifal/src/core/interfaces/api/IReviewAPI.ts
@@ -3,14 +3,7 @@ import IAPI from './IAPI'
 
 abstract class IReviewAPI extends IAPI {
   abstract fetch(): Promise<Review[]>
-  abstract create(
-    user_id: string,
-    name: string,
-    title_book: string,
-    writer: string,
-    review: string,
-    available: boolean
-  ): Promise<void>
+  abstract create(review: Review): Promise<void>
   abstract search(query: string): Promise<Review[]>
 }
 
