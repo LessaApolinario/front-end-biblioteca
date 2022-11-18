ae3d1f06a9a202f4c755916b6933154c0e709f4c
Lessa Apolinario <lessaapolinario@gmail.com>
2022-11-18 17:21:35 -0300

    Change create comment implementation

--- library-ifal/src/api/CommentAPI.ts
+++ library-ifal/src/api/CommentAPI.ts
@@ -1,8 +1,10 @@
+import Comment from '../core/domain/models/Comment'
 import ICommentAPI from '../core/interfaces/api/ICommentAPI'
 
 class CommentAPI extends ICommentAPI {
-  async create(name: string, email: string, comment: string): Promise<void> {
-    await this.client.post('/api/comments', JSON.stringify({ name, email, comment }), {
+  async create(comment: Comment): Promise<void> {
+    await this.client.post('/api/comments',
+      JSON.stringify(comment.toJSON()), {
       headers: {
         'Content-Type': 'application/json',
       },
