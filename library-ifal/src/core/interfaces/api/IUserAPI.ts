88aa4354a63c5124c29391c3157d55132dc6f131
Lessa Apolinario <lessaapolinario@gmail.com>
2022-11-18 17:35:43 -0300

    Change register method signature

--- library-ifal/src/core/interfaces/api/IUserAPI.ts
+++ library-ifal/src/core/interfaces/api/IUserAPI.ts
@@ -5,7 +5,7 @@ import IAPI from './IAPI'
 abstract class IUserAPI extends IAPI {
   abstract login(credentials: AuthCredentialsDTO): Promise<User>
   abstract logout(id: string, token: string): Promise<void>
-  abstract register(name: string, username: string, email: string, password: string): Promise<void>
+  abstract register(user: User): Promise<void>
   abstract refreshSession(storagedToken: string): void
   abstract destroySession(): void
 }
