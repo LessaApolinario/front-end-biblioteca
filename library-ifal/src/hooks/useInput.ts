eb3d1257ad0a33e662464a9a44f5cd454afc46c7
Lessa Apolinario <lessaapolinario@gmail.com>
2022-12-16 12:03:30 -0300

    Add useInput hook for input validation

---
+++ library-ifal/src/hooks/useInput.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react'
+
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+export function useInput() {
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    const isEmpty = error === ''
+    if (!isEmpty) {
+      toast.error(error, {
+        position: toast.POSITION.TOP_RIGHT
+      })
+      setError('')
+    }
+  }, [error])
+
+  function validate(input: HTMLInputElement | null) {
+    if (input !== null) {
+      const isPasswordLengthValid = input.value?.length < 6
+      const isEmail = input.value.match(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i)
+
+      if (input.value === '') {
+        setError(`O campo de ${input.name} é obrigatório`)
+        return
+      }
+
+      if (input.type === 'password' && isPasswordLengthValid) {
+        setError('O campo de senha precisa ter no mínimo 6 caracteres')
+        return
+      }
+
+      if (input.type === 'email' && isEmail) {
+        setError('Este não é um email válido')
+        return
+      }
+    }
+  }
+
+  return { validate }
+}
