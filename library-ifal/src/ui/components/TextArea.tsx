cb534ee56a1c15da26c4217d9b354a00c0e5e087
Lessa Apolinario <lessaapolinario@gmail.com>
2022-12-18 20:24:53 -0300

    Add ref for TextAea component

--- library-ifal/src/ui/components/TextArea.tsx
+++ library-ifal/src/ui/components/TextArea.tsx
@@ -1,3 +1,5 @@
+import { ForwardedRef, forwardRef } from 'react'
+
 import styles from '../styles/components/TextArea.module.scss'
 
 interface TextAreaProps {
@@ -7,13 +9,14 @@ interface TextAreaProps {
   rows: number
 }
 
-function TextArea(props: TextAreaProps) {
+function TextArea(props: TextAreaProps, ref: ForwardedRef<HTMLTextAreaElement>) {
   return <textarea
     className={styles.container}
     name={props.name}
     id={props.id}
     cols={props.cols}
-    rows={props.rows}></textarea>
+    rows={props.rows}
+    ref={ref}></textarea>
 }
 
-export default TextArea
+export default forwardRef(TextArea)
