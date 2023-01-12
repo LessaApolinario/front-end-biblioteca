9dee13f569d3fabdeb349e9f877ea8f4e83523d3
Lessa Apolinario <lessaapolinario@gmail.com>
2023-01-12 15:20:46 -0300

    Diving responsibilities with components on home page

--- library-ifal/src/ui/pages/HomePage.tsx
+++ library-ifal/src/ui/pages/HomePage.tsx
@@ -1,4 +1,3 @@
-import { ReactNode, createRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { GiTreeBranch } from 'react-icons/gi';
@@ -6,108 +5,18 @@ import { GiTreeBranch } from 'react-icons/gi';
 import Button from '../components/Button';
 import Carousel from '../components/Carousel';
 import Header from '../components/Header';
-import PostComponent from '../components/PostComponent';
-import FlexWrapper from '../components/FlexWrapper';
-import Label from '../components/Label';
-import Input from '../components/Input';
-import ItemsList from '../components/ItemsList';
+import PostsArea from '../components/PostsArea';
 
-import Post from '../../core/domain/models/Post';
-
-import PostBuilder from '../../core/domain/builders/PostBuilder';
+import { useAuth } from '../../hooks/useAuth';
 
 import styles from '../styles/pages/HomePage.module.scss';
 
-import { usePosts } from '../../hooks/usePosts';
-import { useAuth } from '../../hooks/useAuth';
-import { useNotifications } from '../../hooks/useNotifications';
-import { useFields } from '../../hooks/useFields';
-
 function HomePage() {
+  const { isAuthenticated, logout } = useAuth();
   const navigate = useNavigate();
-  const formRef = createRef<HTMLFormElement>();
-  const titleRef = createRef<HTMLInputElement>();
-  const contentRef = createRef<HTMLTextAreaElement>();
-  const createPostRef = createRef<HTMLButtonElement>();
-  const [isVisible, setIsVisible] = useState(false);
-  const [text, setText] = useState<string>('Escreva um post');
-
-  const useAuthHook = useAuth();
-  const useFieldsHook = useFields();
-  const useNotificationHook = useNotifications();
-  const usePostsHook = usePosts();
-
-  function buildPost() {
-    return new PostBuilder(useAuthHook.user?.name)
-      .withTitle(titleRef.current?.value)
-      .withContent(contentRef.current?.value)
-      .build();
-  }
-
-  const handleCreatePost = async (event: React.FormEvent) => {
-    event.preventDefault();
-    const titleInput = titleRef.current;
-    const contentTextArea = contentRef.current;
-
-    useFieldsHook.validateInput(titleInput);
-    useFieldsHook.validateTextArea(contentTextArea);
-
-    const post = buildPost();
-
-    try {
-      await usePostsHook.createPost(post);
-    } catch (error: any) {
-      useNotificationHook.notifyError(error.message);
-    }
-
-    setIsVisible(!isVisible);
-    setText('Escreva um post');
-  };
-
-  const renderForm = () => {
-    if (isVisible) {
-      return (
-        <form
-          action="#"
-          ref={formRef}
-          onSubmit={handleCreatePost}
-          className={styles.form}
-        >
-          <FlexWrapper className={styles.postTitle} orientation={'column'}>
-            <Label text={'Título do post'} />
-            <Input type={'text'} name={'título'} ref={titleRef} />
-          </FlexWrapper>
-
-          <FlexWrapper className={styles.postContent} orientation={'column'}>
-            <Label text={'Conteúdo do post'} />
-            <textarea
-              cols={30}
-              rows={10}
-              name={'conteúdo'}
-              ref={contentRef}
-            ></textarea>
-          </FlexWrapper>
-
-          <Button type="submit" btnType="secondary" ref={createPostRef}>
-            Publicar post
-          </Button>
-        </form>
-      );
-    }
-  };
-
-  const changeButtonText = () => {
-    if (!isVisible && text === 'Escreva um post') {
-      setText('Fechar');
-    } else if (isVisible && text === 'Fechar') {
-      setText('Escreva um post');
-    }
-
-    setIsVisible(!isVisible);
-  };
 
   const renderButtons = () => {
-    if (!useAuthHook.isAuthenticated) {
+    if (!isAuthenticated) {
       return (
         <>
           <Button
@@ -132,7 +41,7 @@ function HomePage() {
           type="button"
           btnType="secondary"
           onClick={() => {
-            useAuthHook.logout();
+            logout();
           }}
         >
           Sair
@@ -140,10 +49,6 @@ function HomePage() {
       );
     }
   };
-
-  function renderItem(item: Post): ReactNode {
-    return <PostComponent data={item} />;
-  }
 
   return (
     <main className={styles.container}>
@@ -162,30 +67,8 @@ function HomePage() {
 
       <section className={styles.content}>
         <h1>Bem-vindo</h1>
-
         <Carousel />
-
-        <h3>Área de posts</h3>
-
-        <div className={styles.posts}>
-          <div className={styles.search}>
-            <Button
-              type="button"
-              btnType="secondary"
-              onClick={changeButtonText}
-            >
-              {text}
-            </Button>
-          </div>
-
-          {renderForm()}
-
-          <ItemsList<Post>
-            data={usePostsHook.data}
-            orientation={'column'}
-            renderItem={renderItem}
-          />
-        </div>
+        <PostsArea />
       </section>
     </main>
   );
