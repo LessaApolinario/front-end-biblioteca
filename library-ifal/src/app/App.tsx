6b659cf35bb6bc7658a8fa7d23e8e79ce59ea6b1
Lessa Apolinario <lessaapolinario@gmail.com>
2023-02-12 15:46:20 -0300

    Bringing back routes and contexts to App component

--- library-ifal/src/app/App.tsx
+++ library-ifal/src/app/App.tsx
@@ -1,13 +1,40 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
 import { ToastContainer } from 'react-toastify';
 
-import RouteManager from '../ui/components/RouteManager';
+import AuthProvider from '../ui/contexts/AuthCTX';
 
-import styles from './App.module.scss';
+import BooksPage from '../ui/pages/BooksPage';
+import CommentsPage from '../ui/pages/CommentsPage';
+import Error404Page from '../ui/pages/Error404Page';
+import HintsPage from '../ui/pages/HintsPage';
+import HomePage from '../ui/pages/HomePage';
+import LoginPage from '../ui/pages/LoginPage';
+import RegisterPage from '../ui/pages/RegisterPage';
+import ReviewPage from '../ui/pages/ReviewPage';
+import ReviewDetailsPage from '../ui/pages/ReviewDetailsPage';
+
+import styles from './styles.module.scss';
 
 function App() {
   return (
     <div className={styles.container}>
-      <RouteManager />
+      <BrowserRouter>
+        <AuthProvider>
+          <Routes>
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/books" element={<BooksPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/reviews" element={<ReviewPage />} />
+            <Route path="/reviews/review/:id" element={<ReviewDetailsPage />} />
+            <Route path="/contact" element={<CommentsPage />} />
+            <Route path="/hints" element={<HintsPage />} />
+            <Route path="/" element={<Navigate replace to="/home" />} />
+            <Route path="/*" element={<Error404Page />} />
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
       <ToastContainer />
     </div>
   );
