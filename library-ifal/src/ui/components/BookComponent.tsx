c92ccba7827c0cfeb83a06464c134ca20d8bdbb2
Lessa Apolinario <lessaapolinario@gmail.com>
2023-02-27 12:38:51 -0300

    Using functions to validate data

--- library-ifal/src/ui/components/BookComponent.tsx
+++ library-ifal/src/ui/components/BookComponent.tsx
@@ -3,28 +3,28 @@ import Button from './Button';
 import styles from '../styles/components/BookComponent.module.scss';
 
 import Book from '../../core/domain/models/Book';
+import checkInvalidAPIField from '../../core/utils/checkInvalidAPIField';
 
 interface BookComponentProps {
   props: Book;
 }
 
 function BookComponent({ props }: BookComponentProps) {
-  const formatAuthorName = (authorName: string | undefined) => {
+  function formatAuthorName(authorName?: string) {
     if (!authorName) {
       return '-';
     }
 
-    const [lastName, firstName] = authorName.trim().split(',');
-
+    const [lastName, firstName] = authorName?.trim()?.split(',') ?? [];
     return firstName ? `${firstName} ${lastName}` : lastName;
-  };
+  }
 
   return (
     <tr className={styles.container}>
-      <td>{props.titulo}</td>
-      <td>{formatAuthorName(props.autor)}</td>
-      <td>{props.edicao}</td>
-      <td>{props.ano ? (props.ano = props.ano.replace('.', '')) : '-'}</td>
+      <td>{checkInvalidAPIField(props.titulo)}</td>
+      <td>{checkInvalidAPIField(formatAuthorName(props.autor))}</td>
+      <td>{checkInvalidAPIField(props.edicao)}</td>
+      <td>{checkInvalidAPIField(props.ano)}</td>
       <td>
         <Button
           title="Informações sobre a localização dos livros"
@@ -32,7 +32,7 @@ function BookComponent({ props }: BookComponentProps) {
           btnType="primary"
           onClick={() => console.log('redirecionando')}
         >
-          {props.localizacao}
+          {checkInvalidAPIField(props.localizacao)}
         </Button>
       </td>
     </tr>
--- library-ifal/src/ui/components/ReviewItem.tsx
+++ library-ifal/src/ui/components/ReviewItem.tsx
@@ -1,5 +1,8 @@
 import Review from '../../core/domain/models/Review';
 
+import checkInvalidAPIField from '../../core/utils/checkInvalidAPIField';
+import formatDate from '../../core/utils/formatDate';
+
 import styles from '../styles/components/Review.module.scss';
 
 interface ReviewItemProps {
@@ -8,44 +11,12 @@ interface ReviewItemProps {
 }
 
 function ReviewItem({ data, onClick }: ReviewItemProps) {
-  const abbreviateMonth = (month: string) => {
-    const abbreviatedMonths: Record<string, string> = {
-      '01': 'Ja',
-      '02': 'Fev',
-      '03': 'Mar',
-      '04': 'Abr',
-      '05': 'Maio',
-      '06': 'Jun',
-      '07': 'Jul',
-      '08': 'Ago',
-      '09': 'Set',
-      '10': 'Out',
-      '11': 'Nov',
-      '12': 'Dez',
-    };
-
-    return abbreviatedMonths[month] ?? 'Não existe um mês correspondente';
-  };
-
-  const formatDate = () => {
-    const date = data.created_at?.slice(0, 10);
-    const time = data.created_at?.slice(11, 16);
-
-    if (date && time) {
-      const [hours, minutes] = time?.split(':');
-      const [year, month, day] = date.split('-');
-
-      const abbreviatedMonth = abbreviateMonth(month);
-      return `${abbreviatedMonth} ${day} ${year} às ${hours}:${minutes}`;
-    }
-
-    return '';
-  };
-
   return (
     <article className={styles.container}>
       <h3>
-        {data.name} sobre {data.title_book} de {data.writer}:
+        {checkInvalidAPIField(data.name)} sobre{' '}
+        {checkInvalidAPIField(data.title_book)} de{' '}
+        {checkInvalidAPIField(data.writer)}:
       </h3>
 
       <p
@@ -53,10 +24,12 @@ function ReviewItem({ data, onClick }: ReviewItemProps) {
         title="Clique para ver detalhes da resenha"
         onClick={onClick}
       >
-        {data.review}
+        {checkInvalidAPIField(data.review)}
       </p>
 
-      <p className={styles.date}>{formatDate()}</p>
+      <p className={styles.date}>
+        {formatDate(checkInvalidAPIField(data.created_at))}
+      </p>
     </article>
   );
 }
