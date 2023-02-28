bb7ee9b4d2209e0134de6635cd6782492db23ca6
Lessa Apolinario <lessaapolinario@gmail.com>
2023-02-28 12:29:22 -0300

    Refactor to use refs on useAuth hook

--- library-ifal/src/hooks/useAuth.ts
+++ library-ifal/src/hooks/useAuth.ts
@@ -1,6 +1,8 @@
-import { useContext, useState } from 'react';
+import { createRef, useContext, useState } from 'react';
 
 import User from '../core/domain/models/User';
+
+import UserBuilder from '../core/domain/builders/UserBuilder';
 
 import { AuthCTX } from '../ui/contexts/AuthCTX';
 
@@ -13,9 +15,26 @@ import { useNotifications } from './useNotifications';
 export function useAuth() {
   const { notifySuccess, notifyError } = useNotifications();
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const nameRef = createRef<HTMLInputElement>();
+  const usernameRef = createRef<HTMLInputElement>();
+  const emailRef = createRef<HTMLInputElement>();
+  const passwordRef = createRef<HTMLInputElement>();
+  const confirmPasswordRef = createRef<HTMLInputElement>();
   const authCTX = useContext(AuthCTX);
   const user = authCTX.user;
   const navigate = useNavigate();
+
+  async function handleRegister() {
+    await register(buildUser());
+  }
+
+  function buildUser() {
+    return new UserBuilder(nameRef.current?.value)
+      .withUsername(usernameRef.current?.value)
+      .withEmail(emailRef.current?.value)
+      .withPassword(passwordRef.current?.value)
+      .build();
+  }
 
   async function register(user: User) {
     try {
@@ -27,12 +46,21 @@ export function useAuth() {
     }
   }
 
+  async function handleLogin() {
+    const usernameInput = usernameRef.current;
+    const passwordInput = passwordRef.current;
+    const username = usernameInput?.value ?? '';
+    const password = passwordInput?.value ?? '';
+
+    await login({ username, password });
+  }
+
   async function login({ username, password }: AuthCredentialsDTO) {
     try {
       const success = await authCTX.login({ username, password });
 
       if (success) {
-        setIsAuthenticated(true);
+        setIsAuthenticated(success);
         notifySuccess('Login realizado com sucesso!');
         navigate('/');
       }
@@ -54,9 +82,10 @@ export function useAuth() {
 
   return {
     isAuthenticated,
-    register,
-    login,
+    handleRegister,
+    handleLogin,
     logout,
     user,
+    refs: { nameRef, usernameRef, emailRef, passwordRef, confirmPasswordRef },
   };
 }
