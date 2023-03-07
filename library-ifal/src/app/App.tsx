c07a06154b9aedeaa4d7c72331c3f48379b4810f
Lessa Apolinario <lessaapolinario@gmail.com>
2023-03-07 20:22:05 -0300

    Add new context to manage reviews state

--- library-ifal/src/app/App.tsx
+++ library-ifal/src/app/App.tsx
@@ -4,6 +4,7 @@ import { ToastContainer } from 'react-toastify';
 
 import AuthProvider from '../ui/contexts/AuthCTX';
 import BookProvider from '../ui/contexts/BookCTX';
+import ReviewProvider from '../ui/contexts/ReviewCTX';
 
 import BooksPage from '../ui/pages/BooksPage';
 import CommentsPage from '../ui/pages/CommentsPage';
@@ -22,23 +23,25 @@ function App() {
     <div className={styles.container}>
       <BrowserRouter>
         <BookProvider>
-          <AuthProvider>
-            <Routes>
-              <Route path="/home" element={<HomePage />} />
-              <Route path="/books" element={<BooksPage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegisterPage />} />
-              <Route path="/reviews" element={<ReviewPage />} />
-              <Route
-                path="/reviews/review/:id"
-                element={<ReviewDetailsPage />}
-              />
-              <Route path="/contact" element={<CommentsPage />} />
-              <Route path="/hints" element={<HintsPage />} />
-              <Route path="/" element={<Navigate replace to="/home" />} />
-              <Route path="/*" element={<Error404Page />} />
-            </Routes>
-          </AuthProvider>
+          <ReviewProvider>
+            <AuthProvider>
+              <Routes>
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/books" element={<BooksPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/reviews" element={<ReviewPage />} />
+                <Route
+                  path="/reviews/review/:id"
+                  element={<ReviewDetailsPage />}
+                />
+                <Route path="/contact" element={<CommentsPage />} />
+                <Route path="/hints" element={<HintsPage />} />
+                <Route path="/" element={<Navigate replace to="/home" />} />
+                <Route path="/*" element={<Error404Page />} />
+              </Routes>
+            </AuthProvider>
+          </ReviewProvider>
         </BookProvider>
       </BrowserRouter>
       <ToastContainer />
