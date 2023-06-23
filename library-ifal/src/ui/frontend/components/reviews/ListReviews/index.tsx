0f6646028e416f300d617e66d3cdad18fbff5640
Lessa Apolinario <lessaapolinario@gmail.com>
2023-06-23 20:18:48 -0300

    Remove unused fetch and useEffect call (#23)

--- library-ifal/src/ui/frontend/components/reviews/ListReviews/index.tsx
+++ library-ifal/src/ui/frontend/components/reviews/ListReviews/index.tsx
@@ -1,16 +1,11 @@
-import { useEffect } from 'react';
 import Review from '../../../../../core/domain/models/Review';
 import { useReviews } from '../../../../../hooks/useReviews';
 import { ReviewCard } from '../ReviewCard';
 import styles from './styles.module.scss';
 
 function ListReviews() {
-  const { fetch, getReviews } = useReviews();
+  const { getReviews } = useReviews();
   const { reviews } = getReviews();
-
-  useEffect(() => {
-    fetch();
-  }, []);
 
   function renderItem(review: Review) {
     return <ReviewCard props={review} key={review._id} />;
